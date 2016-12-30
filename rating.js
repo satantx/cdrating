6c68d31d2e2ebafdb351b6a85eec041e15eed34f
satantx <satantx@gmail.com>
2016-12-30 16:00:16 +0300

    Update rating.js

--- rating.js
+++ rating.js
@@ -1,45 +1,48 @@
-$.fn.cdrating = function(options) {
-
+$.fn.StarRating = function(options)
+{
 	var options = $.extend({
-		className: 'cd-star',
-		classActive: 'active'
-	},options);
-
-	return this.each(function(){
+		className: 'cad-star',
+		classActive: 'active',
+		classHover: 'cad-hover',
+		default_stars_cnt: 5
+	},
+	options);
+	return this.each(function()
+	{
 		var doc = $(this),
-				data_options = doc.data(),
-				options_rate = data_options.rate,
-				options_mode = data_options.mode,
-				name_input = data_options.nameInput;
-
-		for (var i = 0; i < 5; i++) {
-			$("<div class='"+options.className+"'>&#9733;</div>").appendTo(doc);
-		};
-
-		doc.removeAttr('data-rate');
-
-		if (options_rate > 0) $('.'+options.className,this).slice(0, options_rate).addClass(options.classActive);
-
-		if (options_mode == 'rating') {
-
-			$("<input type='hidden' name='"+name_input+"'/>").appendTo(doc);
-			doc.removeAttr('data-name-input data-mode');
-
-			doc.addClass('grab');
-
-			$('.'+options.className,doc).hover(function() {
+			data = doc.data(),
+			rate = parseInt(data.value, 10),
+			mode = data.mode,
+			name = data.name,
+			stars_cnt = parseInt(data.starsCnt, 10);
+		doc.removeAttr('data-stars-cnt data-value');
+		if(!stars_cnt) stars_cnt = options.default_stars_cnt;
+		if(!rate) rate = 0;
+		else if(rate > stars_cnt) rate = stars_cnt;
+		for(var i = 0; i < stars_cnt; i++)
+			$('<div class="' + options.className + '">&#9733;</div>').appendTo(doc);
+		if(rate > 0) $('.' + options.className,this).slice(0, rate).addClass(options.classActive);
+		if(mode == 'active')
+		{
+			var input = $('<input type="hidden" name="' + name + '"/>').appendTo(doc);
+			doc.removeAttr('data-name data-mode').addClass('grab');
+			var item = $('.' + options.className, doc);
+			item.hover(
+				function()
+				{
+					var index = $(this).index() + 1;
+					item.slice(0, index).addClass(options.classHover);
+				},
+				function()
+				{
+					item.removeClass(options.classHover);
+				}
+			).click(function()
+			{
 				var index = $(this).index() + 1;
-				$('.'+options.className,doc).slice(0, index).addClass('cd-hover');
-			}, function() {
-				$('.'+options.className,doc).removeClass('cd-hover');
-			});
-
-			$('.'+options.className,doc).click(function() {
-				var index = $(this).index() + 1;
-				$('.'+options.className,doc).removeClass(options.classActive).slice(0, index).addClass(options.classActive);
-				$('input', doc).val(index);
+				item.removeClass(options.classActive).slice(0, index).addClass(options.classActive);
+				input.val(index);
 			});
 		}
-
 	});
 };
